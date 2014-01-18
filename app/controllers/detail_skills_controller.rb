90631ad49d906b9442e5189240a4fc7cbdeb8f2f
quanghung <quanghung89bn@gmail.com>
2014-01-18 16:49:57 +0700

    fix allllll

--- app/controllers/detail_skills_controller.rb
+++
@@ -1,9 +0,0 @@
-class DetailSkillsController < ApplicationController
-# before_action :signed_in_user
-
-  def index
-    user = User.find params[:user_id]
-    @skill_details = user.skill_details.all
-    @skills = Skill.all
-  end
-
--- app/models/detail_skill.rb
+++
@@ -1,5 +0,0 @@
-class DetailSkill < ActiveRecord::Base
-   has_many :user_skills
-  has_many :user, through: :user_skills
-  belongs_to :skill
-end
